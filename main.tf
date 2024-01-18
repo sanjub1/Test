0a755284efa93ce3808059def9b43c67dfe056cf
sanjub1 <156220297+sanjub1@users.noreply.github.com>
2024-01-18 11:49:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "us-east-1" # Change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "hello_world_bucket" {
+resource "aws_s3_bucket_website_configuration" "hello_world_bucket" {
   bucket = "sreyo-helloworld-bucket007" # Replace with your desired bucket name
   acl    = "private" # Set the ACL to "private" to avoid the InvalidBucketAclWithObjectOwnership error
   website {
